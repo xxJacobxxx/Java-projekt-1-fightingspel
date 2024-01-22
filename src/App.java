bcbac1727d08ea508ac8bc25a0a01cb11b274138
Jacob <jacob.hansen@elev.ga.ntig.se>
2024-01-22 08:46:49 +0100

    Första kommiten

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String spela = "ja";
+        while (spela.equalsIgnoreCase("ja")) {
+            System.out.println("Du befinner dig på en båt påväg mot sydamerikas regnskogar. Som praktikant arbete ska du studera Dendrobatidae grodan, även känd som pilgiftsgrodan. Efter flera timmar av konstant tom horisont kommer en ");
+            
+        }
+    }
+}
