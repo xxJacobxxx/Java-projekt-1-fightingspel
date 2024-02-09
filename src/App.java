1b0f9694e1839b7d450ba5a9fbef6fa7ad98e7e7
Jacob <jacob.hansen@elev.ga.ntig.se>
2024-02-09 14:30:16 +0100

    Ändrat valen så programmet inte kraschar om man skriver en bokstav

--- src/App.java
+++ src/App.java
@@ -15,34 +15,38 @@ public class App {
             //Thread.sleep(15000);
             System.out.println("Det är tufft i skogarna. säger han. Jag kan ge dig en liten boost av styrka med mina hemgjorda steroider eller så kan du få lite kunskap för att verkligen förstå allt som händer i skogen.");
             int val1=0;
-            while (val1!=1 || val1!=2 || val1!=3) {
+            while (val1==0) {
                 System.out.println("Väljer du 1: styrkan, 2: kunskapen eller 3: vill du försöka bli vän med kemisten?");
-                System.out.println("Svara i siffror");
-                val1=tb.nextInt();
-                if (val1==1) {
-                    break;
+                
+                String inval1=tb.nextLine();
+                try {
+                    val1 = Integer.parseInt(inval1);
+                } catch (Exception e) {
+                    System.out.println("Svara i siffror");
+                    val1=0;
                 }
-                else if (val1==2){
-                    break;
-                }
-                else if (val1==3){}
-                break;
+                if (val1>4 || val1<0) {
+                    System.out.println("Svara med de gällande siffrorna: 1, 2 eller 3");
+                    val1=0;
             }
+        }
             System.out.println("Efter kemisten givit dig det du bett om tar du dig ner till under deck, en konsekvens av att vara uttråkad. Vapenrummet är fullt med, ja vapen i alla former. Dina ögon dras först till ett svärd. Svärdet verkar vara av asiatiskt ursprung och glänser när solen speglas av det. Till vänster om svärdet har någon sattit upp en flintlock pistol på väggen. Den hade varit dödlig om skotten träffade tänker du medans dina ögon seglar mot elefanten i rummet. Kanonen står ståtligt, laddad och redo för att sänka skepp.");
             System.out.println("Du bestämmer dig att ta med ett vapen till skogen.");
             int val2=0;
-            while (val2!=1 || val2!=2 || val2!=3) {
+            while (val2==0) {
                 System.out.println("Väljer du 1: svärdet, 2: pistolen eller 3: vill du försöka ta med kanonen till skogen?");
-                System.out.println("Svara i siffror");
-                val2=tb.nextInt();
-                if (val2==1) {
-                    break;
+                
+                String inval2 = tb.nextLine();
+                try {
+                    val2 = Integer.parseInt(inval2);
+                } catch (Exception e) {
+                    System.out.println("Svara i siffor");
+                    val2=0;
                 }
-                else if (val2==2){
-                    break;
+                if (val2>4 || val2<0) {
+                    System.out.println("Svara med de gällande siffrorna: 1, 2 eller 3");
+                    val2=0;
                 }
-                else if (val2==3){}
-                break;
             }
             System.out.println("Efter du valt ditt vapen svartntar allt för ögonen och du kollapsar. När du väl kan öppna ögenon hänger du över en öppen eld och ska bli uppäten av en kannibal. Du lyckas göra dig fri och nu är det upp till dig att överleva. Lycka till");
             System.out.println("");
